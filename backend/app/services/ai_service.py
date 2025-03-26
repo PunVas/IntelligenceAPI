bff34158aba8e0b1178c8e26c449a457bd91028d
PunVas <vaswaniusham2212@gmail.com>
2025-03-26 09:28:09 +0530

    the response from now on will come in a json format for recycle

--- backend/app/services/ai_service.py
+++ backend/app/services/ai_service.py
@@ -159,7 +159,7 @@ def decide_recycle_or_resell(product_name: str, product_desc: str, user_answers:
         guide_response = client.models.generate_content(
             model="gemini-2.0-flash", contents=[guide_prompt, user_input]
         )
-        guide_text = guide_response.text if hasattr(guide_response, "text") else str(guide_response).strip()
+        guide_text = guide_response.text if hasattr(guide_response, "text") else str(guide_response).strip("```").strip("json")
 
         # try:
         #     guide_json = json.loads(guide_text)
